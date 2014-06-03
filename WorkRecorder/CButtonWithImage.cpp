ae2edd19fca05e715c36342983b15c65e29838a1
Dmitry Zakablukov <dmitriy.zakablukov@gmail.com>
2014-06-03 23:59:43 +0400

    GNU LGPL license added

--- WorkRecorder/CButtonWithImage.cpp
+++ WorkRecorder/CButtonWithImage.cpp
@@ -1,5 +1,26 @@
-// Button with image control class
-// Fixes disadvantages of CBitmapButton
+// 
+// CButtonWithImage.cpp
+// Button with image control class.
+// Fixes disadvantages of CBitmapButton.
+//
+// This file is part of Work Recorder plugin for Notepad++.
+// Copyright (c) Dmitry Zakablukov, 2013-2014.
+// E-mail: dmitriy.zakablukov@gmail.com
+// Web: https://github.com/BrainHacker/WorkRecorderNpp
+//
+// Work Recorder plugin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Work Recorder plugin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Work Recorder plugin. If not, see <http://www.gnu.org/licenses/>.
+//
 
 #include "common.h"
 
--- WorkRecorder/RecordingWindow.h
+++ WorkRecorder/RecordingWindow.h
@@ -1,4 +1,24 @@
-// Record tab
+// 
+// RecordingWindow.h
+//
+// This file is part of Work Recorder plugin for Notepad++.
+// Copyright (c) Dmitry Zakablukov, 2013-2014.
+// E-mail: dmitriy.zakablukov@gmail.com
+// Web: https://github.com/BrainHacker/WorkRecorderNpp
+//
+// Work Recorder plugin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Work Recorder plugin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Work Recorder plugin. If not, see <http://www.gnu.org/licenses/>.
+//
 
 #pragma once
 
